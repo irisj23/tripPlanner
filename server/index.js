12d5f909badc1e0c46ff0e899d92089bd74570dc
Iris <irisxjiang@gmail.com>
2021-07-06 19:34:27 -0700

    set up directory

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const morgan = require('morgan');
+const path = require('path')
+// const router = require('./routes.js');
+// const db = require('../db');
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+// app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port: ${PORT}`);
+});
