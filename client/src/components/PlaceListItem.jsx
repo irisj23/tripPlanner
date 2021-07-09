e66b881529f4490a06228d1f16a353fc14bf7be9
Iris <irisxjiang@gmail.com>
2021-07-09 01:06:26 -0700

    start notes functionality for each place

--- client/src/components/PlaceListItem.jsx
+++ client/src/components/PlaceListItem.jsx
@@ -4,9 +4,47 @@ import React, { useState } from 'react';
 function PlaceListItem(props) {
   // console.log('PLACE HERE')
   // console.log(props.place)
+  const [click, setClick] = useState(false);
+  const [showNotes, setShowNotes] = useState(false);
+  const [notes, setNotes] = useState('');
+
+  const handleAddNote = () => {
+
+  };
+
+  const handleEditNote = () => {
+
+  };
+
+  const handleDeleteNote = () => {
+
+  };
+
+  // const buttonText = notes.length > 1 ? 'edit notes' : 'add note';
+  // const showNotes = notes.length > 0 ? true : false;
+
+
   return (
     <>
-     <li>{props.place}</li>
+     <li onClick={() => setClick(!click)}>{props.place}</li>
+
+      {click && !showNotes &&  (
+        <>
+        <button>add note</button>
+        </>
+      )}
+
+      {/* {click && notes.length > 1 && (
+        <>
+        <div>clicked here</div>
+
+        <div>notes:</div>
+        <span></span>
+        <button>edit note</button>
+        <button>delete note</button>
+
+        </>
+      )} */}
     </>
   )
 }
